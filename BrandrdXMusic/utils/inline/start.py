326caf9a90eb57c716ca9bce796e23c443b7d82a
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-10 14:44:35 +0530

    Update start.py

--- BrandrdXMusic/utils/inline/start.py
+++ BrandrdXMusic/utils/inline/start.py
@@ -10,10 +10,11 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
         ],
     ]
     return buttons
+
 
 
 def private_panel(_):
@@ -23,11 +24,14 @@ def private_panel(_):
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
-        ],      
-        [InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT)],
-        [
-       InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
-[InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],        
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
+      ],
+        [
+
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+        ],
     ]
     return buttons
